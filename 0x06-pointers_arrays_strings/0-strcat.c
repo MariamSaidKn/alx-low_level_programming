a743249ee5846adb4a85a6744d7a48397c388f93
MariamSaidKn <markhaldoun@gmail.com>
2023-03-30 00:46:55 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,17 +3,9 @@
  * _strcat - char value
  * return: 0
  */
-void char *_strcat(char *dest, char *src);
+void *_strcat(char *dest, char *src);
 {
 	char s1[98] = "Hello ";
 	char s2[] = "World!\n";
 	char *ptr;
-
-	printf("%s\n", s1);
-	printf("%s", s2);
-	ptr = _strcat(s1, s2);
-	printf("%s", s1);
-	printf("%s", s2);
-	printf("%s", ptr);
-	return (0);
 }
